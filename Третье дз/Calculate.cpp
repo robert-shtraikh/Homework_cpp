8047599fe6156fcd0fd8a3f4a1d2b9b191ff873d
Robert Shtraikh <shtraikh.rs@phystech.edu>
2021-09-26 17:22:49 +0300

    Обновил третье дз

---
+++ Третье дз/Calculate.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <math.h>
+#include <vector>
+#include <functional>
+
+
+double calculate(double x, double y, std::function<double(double x, double y)> f)
+{
+    return f(x, y);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    auto f = [](double x, double y) {return x + y; };   //auto - Компилятор сам определяет тип переменной
+    auto g = [](double x, double y) {return x - y; };
+    auto t = [](double x, double y) {return x * y; };
+    auto z = [](double x, double y) {return x / y; };
+    auto h = [](double x, double y) {return pow(x, y); };
+    std::vector<std::function<double(double x, double y)>> vec;    //вектор из функций
+    vec.push_back(f);
+    vec.push_back(g);
+    vec.push_back(t);
+    vec.push_back(z);
+    vec.push_back(h);
+    
+    double x, y, result;
+    std::cout << "Введите x: ";
+    std::cin >> x;
+    std::cout << "Введите y: ";
+    std::cin >> y;
+    for (int i = 0; i < 5; i++) {
+        if (i == 0)
+            std::cout << "x + y = ";
+        if (i == 1)
+            std::cout << "x - y = ";
+        if (i == 2)
+            std::cout << "x * y = ";
+        if (i == 3)
+            std::cout << "x / y = ";
+        if (i == 4)
+            std::cout << "x ^ y = ";
+        result = calculate(x, y, vec[i]);
+        std::cout << result << std::endl;
+    }
+}
+
---
+++ Третье дз/SortAscendingDescending.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+int main()
+{
+	setlocale(LC_ALL, ""); //кириллица
+	std::vector<int> array;
+	int n, input;
+	double elem;
+	std::cout << "Введите количество элементов: ";
+	std::cin >> n;
+	std::cout << "Введите элементы" << std::endl;
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> elem;
+		array.push_back(elem);
+	}
+	std::cout << "Массив до сортировки" << std::endl;
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "Какую сортировку произвести?" << std::endl;
+	std::cout << "1. По возрастанию; " << "2. По убыванию" << std::endl;
+	std::cin >> input;
+	switch(input)
+	{
+	case 1:
+		std::sort(array.begin(), array.end(), [](int a, int b) {return (a < b); });
+		break;
+	case 2:
+		std::sort(array.begin(), array.end(), [](int a, int b) {return (a > b); });
+		break;
+	}
+	std::cout << "Отсортированный массив" << std::endl;
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << array[i] << " ";
+	}
+}
+
