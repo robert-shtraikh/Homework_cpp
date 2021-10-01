c315ab30dbc48d4f22741e1599323571607d1099
Robert Shtraikh <shtraikh.rs@phystech.edu>
2021-10-01 20:34:49 +0300

    Добавил сортировку с аргументами через консоль

---
+++ Второе дз/MergeSort.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+void TopDownMerge(int iBegin, int iMiddle, int iEnd, std::vector <int>& A)
+{
+	int i = iBegin, j = iMiddle;
+	int B_COPY = iEnd - iBegin;
+	std::vector <int> B(B_COPY);
+	for (auto k = 0; k < B_COPY; k++)
+	{
+		if (i < iMiddle && (j >= iEnd || A[i] <= A[j]))
+		{
+			B[k] = A[i];
+			i++;
+		}
+		else
+		{
+			B[k] = A[j];
+			j++;
+		}
+	}
+	for (auto j = 0; j < B_COPY; ++j)
+	{
+		A[static_cast<std::vector<int, std::allocator<int>>::size_type>(iBegin) + j] = B[j];
+	}
+}
+
+void TopDownSplitMerge(int left, int right, std::vector <int>& A)
+{
+	if (right - left <= 1)
+	{
+		return;
+	}
+	int middle = (left + right) / 2;
+	TopDownSplitMerge(left, middle, A);
+	TopDownSplitMerge(middle, right, A);
+	TopDownMerge(left, middle, right, A);
+}
---
+++ Второе дз/MergeSortWithConsole.cpp
@@ -0,0 +1,31 @@
+﻿// MergeSortWithConsole.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include "MergeSort.cpp"
+
+int main(int argc, char** A_char)		//A_char = массив символов
+{
+	std::vector<std::string> A_string;	//строка
+	//const char** filePtr = fileList;
+	for (int i = 0; i < argc; i++)
+		A_string.push_back(A_char[i]);
+
+	//std::vector <int> A;
+	//for (int i = 0; i < argc; i++)
+		//A.push_back(int(A_char[i]));		
+	std::vector<int> A_int;			//массив целых чисел
+	for (int i = 0; i < argc; i++)
+	{
+		int num = atoi(A_string.at(i).c_str());
+		A_int.push_back(num);
+	}
+
+
+	TopDownSplitMerge(0, A_int.size(), A_int);
+	for (auto i : A_int) { std::cout << i << " "; };
+	return 0;
+}
+//g++ -o merge.exe MergeSortWithConsole.cpp
